8039f29e0acff10717599d6d88ff0eb829826464
zaystore242 <yudaaguz099@gmail.com>
2025-11-13 16:56:24 +0800

    Update atlakey in setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
 const domain = "https://syahadpprivate.anti-ddos.me"
 const apikey = "ptla_KqduGtbk1NwOKysQHplpIqyBRvMFkJP5QGzYHo2tRID"
-const atlakey = "zRL6jwt9dhbHV8yesYrCohvJTYu4pV9j5MbuhsFduiyIFnrZpbgfxNTWan6BTVS8IdXCsrpkueQlj3CBLxWgtrot97kIvbgDLhwN"
+const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
