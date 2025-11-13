cd3b492511a5b15982c0c6dc7096fb6f383b1bbd
zaystore242 <yudaaguz099@gmail.com>
2025-11-13 16:35:53 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
 const domain = "https://syahadpprivate.anti-ddos.me"
 const apikey = "ptla_KqduGtbk1NwOKysQHplpIqyBRvMFkJP5QGzYHo2tRID"
-const atlakey = ""
+const atlakey = "zRL6jwt9dhbHV8yesYrCohvJTYu4pV9j5MbuhsFduiyIFnrZpbgfxNTWan6BTVS8IdXCsrpkueQlj3CBLxWgtrot97kIvbgDLhwN"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
