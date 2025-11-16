c075256702c265aaede7ce8a9561fc8c507f610a
zaystore242 <yudaaguz099@gmail.com>
2025-11-16 12:05:18 +0800

    Update API configuration in setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://syahadpprivate.anti-ddos.me"
-const apikey = "ptla_KqduGtbk1NwOKysQHplpIqyBRvMFkJP5QGzYHo2tRID"
+const domain = "https://izumi.vera.hosting.naell.my.id"
+const apikey = "ptla_UIdw8naWqBnm3Un0r4aViRn7c19N36f2Cp7kDNWBowy"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
