303fba0a3eda6b19c2b057ebb48829f79323b1fa
zaystore242 <yudaaguz099@gmail.com>
2025-11-16 15:39:34 +0800

    Update API keys and domain in setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://izumi.vera.hosting.naell.my.id"
-const apikey = "ptla_UIdw8naWqBnm3Un0r4aViRn7c19N36f2Cp7kDNWBowy"
+const domain = "https://fyzzgantenk.wibusoft.my.id"
+const apikey = "ptla_xpCIvNGkwUGFJ7lM5RHVDGp6edIQsfxUoMvO4n6Di8i"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
