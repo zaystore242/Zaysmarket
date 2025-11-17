81fb9de7cfba1eb367684639412fa08d98ac7954
zaystore242 <yudaaguz099@gmail.com>
2025-11-17 10:10:36 +0800

    Update API keys in setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
 const domain = "https://zaystore.zakzz.my.id"
-const apikey = "ptla_2cDc1V67FDCZ6djapayLxiVBclAdsN0QMGvn97IBUnE"
+const apikey = "ptla_CUl2ySxNfXxsIbgHOz88KaJSD5d8Ib4EZhfQbd0pQBc"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
