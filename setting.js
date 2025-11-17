3dcd8c2be8d0c33b859947a42c8ab3713fa8e7cf
zaystore242 <yudaaguz099@gmail.com>
2025-11-17 10:04:13 +0800

    Update API configuration with new domain and key

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://fyzzgantenk.wibusoft.my.id"
-const apikey = "ptla_xpCIvNGkwUGFJ7lM5RHVDGp6edIQsfxUoMvO4n6Di8i"
+const domain = "https://zaystore.zakzz.my.id"
+const apikey = "ptla_2cDc1V67FDCZ6djapayLxiVBclAdsN0QMGvn97IBUnE"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
