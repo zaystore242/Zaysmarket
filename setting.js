e600ce5870eb6859b2bc18cf3506256d66360e2e
zaystore242 <yudaaguz099@gmail.com>
2025-11-23 15:28:59 +0800

    Update API keys and domain in setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://syahnihdeks.anti-ddos.me"
-const apikey = "ptla_zrCJJHZI2f29x8rMlNIcKkSWqSs3HfnCWz4GjYFnEvV"
+const domain = "https://syahnihbang.storedigital.web.id"
+const apikey = "ptla_oxmJmxQy80QcoWUvopeMdQXQf95JR3lOnzTPsF76w8M"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
