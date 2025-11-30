e8ed92c69f814494d4bea32122139acf0095547a
zaystore242 <yudaaguz099@gmail.com>
2025-11-30 17:12:44 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://fyzz.web.raflydev.my.id"
-const apikey = "ptla_1D72g8MMvfnGMKrchgUsxwbaYUIgtUsIcjujVOIxZid"
+const domain = "https://syahgantengbetjir.storedigital.web.id"
+const apikey = "ptla_oaal098JnlDdQqUH4CLo3GbKD8dxFEqh1L78MSZPoqU"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
