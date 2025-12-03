985d9406cddec0e576bc70b4ad199d67a8e2aba4
zaystore242 <yudaaguz099@gmail.com>
2025-12-04 05:08:07 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://syahgantengbetjir.storedigital.web.id"
-const apikey = "ptla_oaal098JnlDdQqUH4CLo3GbKD8dxFEqh1L78MSZPoqU"
+const domain = "https://alluffypanelatos.shopryzen.my.id"
+const apikey = "ptla_cAFyYiFmq3OSCTbuzdT1M14JNqCTc6CeOPY50Ka0Hu8"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
