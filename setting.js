dafcbe14570992700e038aa3999a5051d39cce34
zaystore242 <yudaaguz099@gmail.com>
2025-12-04 20:13:34 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://yogzzzstr-publik.cloudxyz.web.id"
-const apikey = "ptla_fCuESdmY8MYRiA94uSsrpu1wWkhmm24mWSjvnLmdCLS"
+const domain = "https://privat.salxxoffc.biz.id"
+const apikey = "ptla_gKKplb967d2HzAWt4NTRFpSH6GQeGJVkOLrBpydoREF"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
