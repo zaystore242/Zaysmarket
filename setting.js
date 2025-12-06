a7cf31592f5086e8101614a934c850fdf8aa1ca6
zaystore242 <yudaaguz099@gmail.com>
2025-12-06 19:56:33 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-const domain = "https://syahpriv.anti-ddos.me"
-const apikey = "ptla_bKxz83JCyw6Xhe5jHCLM58FQiM9edr40v6SnY7qLjpM"
+const domain = "https://panel.kayxzyu.web.id"
+const apikey = "ptla_bgOw1Rxu02uiWsWCldMNbFgZurl3TdFo2zLAYB7ldmd"
 const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
 
